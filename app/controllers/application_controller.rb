25b5c95cbd40c707195d1f00ada019c539d33137
McN33ly <rmcneely23@gmail.com>
2016-05-08 21:06:06 -0400

    added error, sign-in, sign-up pages. Syled site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,29 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   enable :sessions
+  set :session_secret, "password_security"
   set :public_folder, 'public'
   set :views, 'app/views'
 
   get '/' do
+    @locations = Location.all
     erb :home
   end
+
+
+  get '/public/background-1' do
+
+  end
+
+  get '/error' do
+    erb :error
+  end
+
+#  post '/home' do
+#    session[:id] = User.create(params[:user])
+#    #binding.pry
+#    redirect '/'
+#  end
 
   helpers do
     def logged_in?
@@ -17,5 +34,4 @@ class ApplicationController < Sinatra::Base
       User.find(session[:id])
     end
   end
-
 end
--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -8,4 +8,33 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/sign-up' do
+    if !User.find(user_name: params[:user][:user_name])
+      user = User.new(params[:user])
+    else
+      @username_taken
+      redirect '/error'
+    end
+    session[:id] = user.save
+    redirect '/'
+  end
+
+  get '/sign-in' do
+    if !logged_in?
+      erb :sign_in
+    else
+      redirect '/home'
+    end
+  end
+
+  post '/sign-in' do
+    user = User.find(user_name: params[:user][:user_name])
+    if user && user.authenticate(params[:user][:password])
+      session[:id] = user
+      redirect '/home'
+    else
+      redirect '/error'
+
+  end
+
 end
